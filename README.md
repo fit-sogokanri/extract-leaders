e5b8d557ec2690d6c8e3436afc8c2d210db5efb2
SouTamura <s21a2036@bene.fit.ac.jp>
2023-12-06 19:38:35 +0900

    [add]ファイルを追加

---
+++ README.md
@@ -0,0 +1,9 @@
+# Extract Leaders
+
+部員名簿から役職者を抽出する。
+
+## 製作者
+Sou Tamura([tmsou0209](https://github.com/tmsou0209))
+
+## ライセンス
+MITライセンス
---
+++ main.py
@@ -0,0 +1,98 @@
+import openpyxl
+from pathlib import Path
+import csv
+import sys
+import os
+import shutil
+
+# wb = openpyxl.load_workbook('部員名簿-モダンアート部.xlsx', data_only=True)
+# ws = wb["Sheet1"]
+
+# 代表者
+representative = []
+# 会計長
+accountant = []
+# 総務
+general_affairs = []
+# 副代表者
+deputy_representative = []
+# 副会計長
+deputy_accountant = []
+
+
+def read_role(file_name):
+    wb = openpyxl.load_workbook(filename=file_name, data_only=True)
+    ws = wb["Sheet1"]
+    for row in ws.rows:
+        if (row[4].value == "部長・主将" or
+                row[4].value == "会計長" or
+                row[4].value == "総務" or
+                row[4].value == "副部長・副主将" or
+                row[4].value == "副会計長"
+        ):
+            tel_num = row[5].value
+
+            print(file_name)
+            if tel_num and len(str(tel_num)) < 13:
+                number_list = list(str(row[5].value))
+                number_list.insert(3, "-")
+                number_list.insert(8, "-")
+                tel_num = ''.join(number_list)
+
+            row_data = [
+                row[2].value,
+                "",
+                str(row[3].value).upper(),
+                tel_num,
+                ws["D1"].value
+            ]
+            print(row[4].value)
+            if row[4].value == "部長・主将":
+                representative.append(row_data)
+            elif row[4].value == "会計長":
+                accountant.append(row_data)
+            elif row[4].value == "総務":
+                general_affairs.append(row_data)
+            elif row[4].value == "副部長・副主将":
+                deputy_representative.append(row_data)
+            elif row[4].value == "副会計長":
+                deputy_accountant.append(row_data)
+
+
+def create_output_file():
+    leaders_list = [
+        ["代表者", representative],
+        ["会計長", accountant],
+        ["総務", general_affairs],
+        ["副代表者", deputy_representative],
+        ["副会計長", deputy_accountant]
+    ]
+
+    wb = openpyxl.Workbook()
+
+    for leaders in leaders_list:
+        representative_sheet = wb.create_sheet(title=leaders[0])
+        i = 0
+        for row in representative_sheet.iter_rows(min_row=1, min_col=1,
+                                                  max_row=len(leaders[1]),
+                                                  max_col=len(leaders[1][0])):
+            j = 0
+            for cell in row:
+                # print(leaders[1][i][j])
+                cell.value = leaders[1][i][j]
+                j += 1
+            i += 1
+
+    wb.save('output_file.xlsx')
+
+
+dir_path = "member_list"
+
+files = [
+    os.path.join(dir_path, f) for f in os.listdir(dir_path) if os.path.isfile(os.path.join(dir_path, f))
+]
+
+for file_path in files:
+    read_role(file_path)
+
+create_output_file()
